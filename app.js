0e0019ad47885b56356d6d6f3ac091b66079188f
JGusM <jgusmei@gmail.com>
2021-08-10 22:36:03 -0300

    repositorio de prueba

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const path = require("path");
+const port= 3000;
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
 });
 
 app.get('/', (req,res)=>{
@@ -18,3 +23,4 @@ app.get('/login', (req,res)=>{
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
+
